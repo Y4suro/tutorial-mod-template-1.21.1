57885be9b08c63764ef3cd7e4102527d7157a383
yasuro <mathis.provent.13220@gmail.com>
2024-08-23 18:09:44 +0200

    23/08/2024-initial-push

---
+++ src/main/java/net/yasuro/tutorialmod/Tutorialmodclient.java
@@ -0,0 +1,10 @@
+package net.yasuro.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Tutorialmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
